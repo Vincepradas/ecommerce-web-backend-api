7978a6052d4c0d9d686e7fc9d96af100bbb9cb83
User <pradasvince@gmail.com>
2025-01-01 19:50:33 +0800

    asdsa

--- models/Order.js
+++ models/Order.js
@@ -16,22 +16,6 @@ const orderSchema = new mongoose.Schema({
       type: String,
       required: true
     },
-    city: {
-      type: String,
-      required: true
-    },
-    state: {
-      type: String,
-      required: true
-    },
-    zip: {
-      type: String,
-      required: true
-    },
-    country: {
-      type: String,
-      required: true
-    }
   },
   products: [
     {
